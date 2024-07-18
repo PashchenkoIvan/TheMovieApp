e49cd8c77811055fb29e5d29eab25bcad52f5ce2
PashchenkoIvan <ipaschenko09@gmail.com>
2024-07-18 12:21:59 +0300

    Created login view

--- Podfile
+++ Podfile
@@ -2,5 +2,6 @@ target 'TheMovieApp' do
   use_frameworks!
   
   pod 'Alamofire'
+  pod 'Kingfisher'
 
 end
--- TheMovieApp/Modules/Login/View/LoginViewController.swift
+++ TheMovieApp/Modules/Login/View/LoginViewController.swift
@@ -6,24 +6,36 @@
 //
 
 import UIKit
+import Kingfisher
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var movieTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        movieTextLabel.backgroundColor = UIColor.clear
+        movieTextLabel.layer.borderColor = UIColor.white.cgColor
+        movieTextLabel.layer.borderWidth = 2
+        
+        let gradient = CAGradientLayer()
+        gradient.frame = imageView.bounds
+        gradient.colors = [UIColor.black.cgColor, UIColor.black.cgColor, UIColor.clear.cgColor, UIColor.black.cgColor, UIColor.black.cgColor]
+        gradient.locations = [0.0, 0.1, 0.5, 0.9, 1.0]
+        imageView.layer.insertSublayer(gradient, at: 0)
+        
+        let url = URL(string: "https://i.pinimg.com/564x/20/a4/23/20a4239750efa6d888960faafd1e8708.jpg")
+        imageView.kf.setImage(with: url)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonPressed(_ sender: Any) {
+        
     }
-    */
-
+    
 }
