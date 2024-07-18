9c80f6e0b606d78dfcbf4d20532b5a68809a8559
PashchenkoIvan <ipaschenko09@gmail.com>
2024-07-18 16:17:18 +0300

    Create network helper class

--- TheMovieApp/Helpers/NetworkHelper/NetworkHelper.swift
+++ TheMovieApp/Helpers/NetworkHelper/NetworkHelper.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import Network
+
+class NetworkHelper {
+    static func hasInternetConnection() -> Bool {
+        let monitor = NWPathMonitor()
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        var isConnected = false
+        
+        monitor.pathUpdateHandler = { path in
+            isConnected = path.status == .satisfied
+            semaphore.signal()
+        }
+        
+        monitor.start(queue: queue)
+        semaphore.wait() // Ожидаем сигнала от pathUpdateHandler
+        monitor.cancel() // Останавливаем мониторинг после получения статуса
+        
+        return isConnected
+    }
+}
